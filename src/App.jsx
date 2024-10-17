cc276cdab78baa219d331a45679ba3e0dcec6c58
Dipannita_Mondal <dipannitamisti10@gmail.com>
2024-10-17 23:10:57 +0530

    background created'

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import List from './list/List'
+import Chat from './chat/Chat'
+import Detail from './detail/Detail'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-4xl bold'>App</div>
+    <div className="bg-[url('./assets/bg.jpg')]  bg-cover bg-center bg-no-repeat h-screen flex items-center justify-center ">
+      <div className="container inset-0 backdrop-blur-3xl backdrop-saturate-[180%] bg-blue-950/50 p-6 rounded-lg flex items-center justify-center ">
+        <div className='text-white flex '>
+           <List/>
+           <Chat/>
+           <Detail/>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,16 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  variants: {
+    extend: {
+      backdropBlur: ['responsive', 'hover', 'focus'],
+      backdropSaturate: ['responsive', 'hover', 'focus'],
+    },
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    
+  ],
 }
