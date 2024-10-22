e9736d954b6b0eb733932d389bb9c61d0d0f4db1
Dipannita_Mondal <dipannitamisti10@gmail.com>
2024-10-22 15:39:49 +0530

    chatsection added

--- src/chat/Chat.jsx
+++ src/chat/Chat.jsx
@@ -37,12 +37,90 @@ const Chat = () => {
 
       </div>
 
-      <div className="center">
-        <h1>center</h1>
+      <div className="center border-b border-gray-400 flex flex-col flex-1 p-5 gap-4 overflow-y-scroll">
+        <div className="message flex gap-2 w-[70%]">
+          <img src={avatar} alt="avtar" className='rounded-full h-10 w-10'/>
+          <div className='bg-slate-200 text-blue-950 rounded-xl p-3'>
+          <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quidem nihil fugiat aut est! Error quisquam 
+          unde ducimus accusamus quis cupiditate aspernatur delectus obcaecati, fugit temporibus possimus repellendus. Vitae, sapiente?
+          </p>
+          <span className='text-xs flex float-end'>11.11 am</span>
+          </div>
+        </div>
+
+        <div className="message-own flex justify-end w-full">
+          
+        <div className='text-slate-200 bg-blue-950 rounded-xl p-3 w-[60%]' >
+          <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quidem nihil fugiat aut est! Error quisquam 
+          unde ducimus accusamus quis cupiditate aspernatur delectus obcaecati, fugit temporibus possimus repellendus. Vitae, sapiente?
+          </p>
+          <span className='text-xs flex float-end'>11.11 am</span>
+          </div>          
+        </div>
+
+        <div className="message flex gap-2 w-[70%]">
+          <img src={avatar} alt="avtar" className='rounded-full h-10 w-10'/>
+          <div className='bg-slate-200 text-blue-950 rounded-xl p-3'>
+          <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quidem nihil fugiat aut est! Error quisquam 
+          unde ducimus accusamus quis cupiditate aspernatur delectus obcaecati, fugit temporibus possimus repellendus. Vitae, sapiente?
+          </p>
+          <span className='text-xs flex float-end'>11.11 am</span>
+          </div>
+        </div>
+
+        <div className="message-own flex justify-end w-full">
+          
+        <div className='text-slate-200 bg-blue-950 rounded-xl p-3 w-[60%]' >
+          <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quidem nihil fugiat aut est! Error quisquam 
+          unde ducimus accusamus quis cupiditate aspernatur delectus obcaecati, fugit temporibus possimus repellendus. Vitae, sapiente?
+          </p>
+          <span className='text-xs flex float-end'>11.11 am</span>
+          </div>          
+        </div>
+
+        <div className="message flex gap-2 w-[70%]">
+          <img src={avatar} alt="avtar" className='rounded-full h-10 w-10'/>
+          <div className='bg-slate-200 text-blue-950 rounded-xl p-3'>
+          <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quidem nihil fugiat aut est! Error quisquam 
+          unde ducimus accusamus quis cupiditate aspernatur delectus obcaecati, fugit temporibus possimus repellendus. Vitae, sapiente?
+          </p>
+          <span className='text-xs flex float-end'>11.11 am</span>
+          </div>
+        </div>
+
+        <div className="message-own flex justify-end w-full">
+          
+        <div className='text-slate-200 bg-blue-950 rounded-xl p-3 w-[60%]' >
+          <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quidem nihil fugiat aut est! Error quisquam 
+          unde ducimus accusamus quis cupiditate aspernatur delectus obcaecati, fugit temporibus possimus repellendus. Vitae, sapiente?
+          </p>
+          <span className='text-xs flex float-end'>11.11 am</span>
+          </div>          
+        </div>
+
+        <div className="message flex gap-2 w-[70%]">
+          <img src={avatar} alt="avtar" className='rounded-full h-10 w-10'/>
+          <div className='bg-slate-200 text-blue-950 rounded-xl p-3'>
+          <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quidem nihil fugiat aut est! Error quisquam 
+          unde ducimus accusamus quis cupiditate aspernatur delectus obcaecati, fugit temporibus possimus repellendus. Vitae, sapiente?
+          </p>
+          <span className='text-xs flex float-end'>11.11 am</span>
+          </div>
+        </div>
+
+        <div className="message-own flex justify-end w-full">
+          
+        <div className='text-slate-200 bg-blue-950 rounded-xl p-3 w-[60%]' >
+          <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quidem nihil fugiat aut est! Error quisquam 
+          unde ducimus accusamus quis cupiditate aspernatur delectus obcaecati, fugit temporibus possimus repellendus. Vitae, sapiente?
+          </p>
+          <span className='text-xs flex float-end'>11.11 am</span>
+          </div>          
+        </div>
 
       </div>
 
-      <div className="bottom flex gap-2 justify-between items-center mr-3 ">
+      <div className="bottom flex gap-2 justify-between items-center mr-3  mt-4">
             <div className="icons flex gap-2 text-xl cursor-pointer">
                   <IoImageOutline/>
                   <IoCameraOutline/>
