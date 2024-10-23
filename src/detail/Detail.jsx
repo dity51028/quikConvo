10b0b29f1ac6ee56a381b238539804138e914f29
Dipannita_Mondal <dipannitamisti10@gmail.com>
2024-10-23 22:00:56 +0530

    details section done

--- src/detail/Detail.jsx
+++ src/detail/Detail.jsx
@@ -1,18 +1,75 @@
 import React from 'react'
 import avatar from '../assets/avatar.png'
+import { FaChevronCircleUp,FaChevronCircleDown } from "react-icons/fa";
+import { IoMdDownload } from "react-icons/io";
+import photo from  '../assets/photo.jpeg'
+
 
 const Detail = () => {
   return (
-    <div className='detail flex flex-1 gap-4'>
-      <div className="top flex flex-col items-center">
+    <div className='detail flex flex-col flex-1 gap-2 '>
+      <div className="top flex flex-col items-center  text-center border-b border-gray-400 pb-6">
         <img src={avatar} alt="" className='rounded-full w-20 h-20' />
-        <h1 className='text-lg font-bold'>John Doe</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+        <h1 className='text-lg font-bold p-2'>John Doe</h1>
+        <p className='text-sm'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
       </div>
 
-      <div className="bottom">
+      <div className="bottom ml-2">
+           <div className="options">
+            <div className="chat flex my-5  justify-between items-center cursor-pointer">
+              <h4 className=''>Chat Settings</h4>
+              <FaChevronCircleUp className='text-gray-800 '/>
+            </div>
+            <div className="privacy my-5 flex justify-between items-center cursor-pointer">
+              <h4 className=''>Privacy & help</h4>
+              <FaChevronCircleUp className='text-gray-800 '/>
+            </div>
 
+            <div className="photo my-5 flex justify-between items-center cursor-pointer">
+              <h4 className=''>Shared Photos</h4>
+              <FaChevronCircleDown className='text-gray-800 '/>
+            </div>
+            <div className="shared-photos my-4 flex justify-between  items-center gap-16">
+              <div className='flex justify-between gap-4'>
+                <img src={photo} alt="" height={24} width={24}/>
+                <h1>photo_2024_1.png</h1>
+              </div>
+              <IoMdDownload/>
+            </div>
+            <div className="shared-photos my-4 flex justify-between  items-center gap-16">
+              <div className='flex justify-between gap-4'>
+                <img src={photo} alt="" height={24} width={24}/>
+                <h1>photo_2024_1.png</h1>
+              </div>
+              <IoMdDownload/>
+            </div>
+            <div className="shared-photos my-4 flex justify-between  items-center gap-16">
+              <div className='flex justify-between gap-4'>
+                <img src={photo} alt="" height={24} width={24}/>
+                <h1>photo_2024_1.png</h1>
+              </div>
+              <IoMdDownload/>
+            </div>
+            <div className="shared-photos my-4 flex justify-between  items-center gap-16">
+              <div className='flex justify-between gap-4'>
+                <img src={photo} alt="" height={24} width={24}/>
+                <h1>photo_2024_1.png</h1>
+              </div>
+              <IoMdDownload/>
+            </div>
+
+            <div className="files flex my-5  justify-between items-center cursor-pointer">
+              <h4 className=''>Shared files</h4>
+              <FaChevronCircleUp className='text-gray-800 '/>
+            </div>
       </div>
+      <div className="buttons flex flex-col gap-4 absolute bottom-12 ml-10 ">
+      <button className='bg-red-400 hover:bg-red-600 px-14 py-2 '>Block User</button>
+      <button className='bg-slate-300 text-black hover:bg-white  px-14 py-2'>Log Out</button>
+      </div>
+      
+
+    </div>
     </div>
   )
 }
