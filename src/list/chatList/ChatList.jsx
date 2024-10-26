592a1389057749bb343296838357ac96cf98a5ae
Dipannita_Mondal <dipannitamisti10@gmail.com>
2024-10-26 15:28:26 +0530

    error in updation chatlist

--- src/list/chatList/ChatList.jsx
+++ src/list/chatList/ChatList.jsx
@@ -1,12 +1,46 @@
-import React, { useState } from 'react'
-import { FaSearch,FaPlus } from "react-icons/fa";
+import React, { useEffect, useState } from 'react'
+import { FaSearch} from "react-icons/fa";
 import { FiPlusCircle,FiMinusCircle } from "react-icons/fi";
 import avatar from '../../assets/avatar.png'
 import AddUser from './addUser/AddUser';
+import { useUserStore } from '../../lib/userStore';
+import { doc, getDoc, onSnapshot } from 'firebase/firestore';
+import { db } from '../../lib/firebase';
 
 
 const ChatList = () => {
-    const [addMode,setAddMode]=useState(false)
+
+    const [addMode,setAddMode]=useState(true)
+    const [chats,setChats] = useState([])
+
+    const {currentUser} = useUserStore()
+
+    useEffect (()=>{
+        const unsub = onSnapshot(doc(db, "userChat", currentUser.id ), 
+        async (res) => {
+           const items = res.data().chats;
+
+           const promises = items.map(async (items)=>{
+              const userDocRef = doc(db, "users", items.receiverId);
+              const userDocSnap = await getDoc(userDocRef);
+
+              const user = userDocSnap.data()
+
+              return {...items,user};
+
+              });
+                const chatData = await Promise.all(promises);
+
+                setChats(chatData.sort((a,b)=>b.updatedAt-a.updatedAt))
+
+        });
+
+        return ()=>{
+            unsub();
+        }
+        
+    },[currentUser.id]);
+
   return (
     
         <div className="chatlist mt-6 flex flex-col flex-1 overflow-y-scroll scrollbar-thin  scrollbar-thumb-blue-950 scrollbar-track-transparent">
@@ -26,86 +60,21 @@ const ChatList = () => {
             </div>
 
             <div className='chatItems  '>
+                {
+                    chats.map(()=>{
+                        <div className="item flex items-center m-6 py-2 gap-4 border-b-2 border-gray-400 pb-3" key={chats.chatId}>
+                        <img src={avatar} alt="avatar" className='rounded-full w-14 h-14' />
+                        <div className='text'>
+                            <h2 className='text-lg'>John Doe</h2>
+                            <p className='text-sm'>{chats.lastMessage}</p>
+                        </div>
+                    </div>
 
-            <div className="item flex items-center m-6 py-2 gap-4 border-b-2 border-gray-400 pb-3">
-                    <img src={avatar} alt="avatar" className='rounded-full w-14 h-14' />
-                    <div className='text'>
-                        <h2 className='text-lg'>John Doe</h2>
-                        <p className='text-sm'>Hello world</p>
-                    </div>
-                </div>
-
-                <div className="item flex items-center m-6 gap-4 border-b-2 border-gray-400 pb-3">
-                    <img src={avatar} alt="avatar" className='rounded-full w-14 h-14'/>
-                    <div className='text'>
-                    <h2 className='text-lg'>John Doe</h2>
-                        <p className='text-sm'>Hello world</p>
-                    </div>
-                </div>
-                
-                <div className="item flex items-center m-6 gap-4 border-b-2 border-gray-400 pb-3">
-                    <img src={avatar} alt="avatar" className='rounded-full w-14 h-14' />
-                    <div className='text'>
-                    <h2 className='text-lg'>John Doe</h2>
-                        <p className='text-sm'>Hello world</p>
-                    </div>
-                </div>
-                
-                <div className="item flex items-center m-6 gap-4 border-b-2 border-gray-400 pb-3">
-                    <img src={avatar} alt="avatar" className='rounded-full w-14 h-14'/>
-                    <div className='text'>
-                    <h2 className='text-lg'>John Doe</h2>
-                        <p className='text-sm'>Hello world</p>
-                    </div>
-                </div>
-                <div className="item flex items-center m-6 gap-4 border-b-2 border-gray-400 pb-3">
-                    <img src={avatar} alt="avatar" className='rounded-full w-14 h-14'/>
-                    <div className='text'>
-                    <h2 className='text-lg'>John Doe</h2>
-                        <p className='text-sm'>Hello world</p>
-                    </div>
-                </div>
-
-                <div className="item flex items-center m-6 py-2 gap-4 border-b-2 border-gray-400 pb-3">
-                    <img src={avatar} alt="avatar" className='rounded-full w-14 h-14' />
-                    <div className='text'>
-                    <h2 className='text-lg'>John Doe</h2>
-                        <p className='text-sm'>Hello world</p>
-                    </div>
-                </div>
-
-                <div className="item flex items-center m-6 gap-4 border-b-2 border-gray-400 pb-3">
-                    <img src={avatar} alt="avatar" className='rounded-full w-14 h-14'/>
-                    <div className='text'>
-                    <h2 className='text-lg'>John Doe</h2>
-                        <p className='text-sm'>Hello world</p>
-                    </div>
-                </div>
-                
-                <div className="item flex items-center m-6 gap-4 border-b-2 border-gray-400 pb-3">
-                    <img src={avatar} alt="avatar" className='rounded-full w-14 h-14' />
-                    <div className='text'>
-                    <h2 className='text-lg'>John Doe</h2>
-                        <p className='text-sm'>Hello world</p>
-                    </div>
-                </div>
-                
-                <div className="item flex items-center m-6 gap-4 border-b-2 border-gray-400 pb-3">
-                    <img src={avatar} alt="avatar" className='rounded-full w-14 h-14'/>
-                    <div className='text'>
-                    <h2 className='text-lg'>John Doe</h2>
-                        <p className='text-sm'>Hello world</p>
-                    </div>
-                </div>
-                <div className="item flex items-center m-6 gap-4 border-b-2 border-gray-400 pb-3">
-                    <img src={avatar} alt="avatar" className='rounded-full w-14 h-14'/>
-                    <div className='text'>
-                    <h2 className='text-lg'>John Doe</h2>
-                        <p className='text-sm'>Hello world</p>
-                    </div>
-                </div>
-                </div>
-                {addMode && <AddUser/>}
+                    })
+                }
+           
+            </div>
+                {(!addMode) && <AddUser/>}
                 
         </div>
     
--- src/list/chatList/addUser/AddUser.jsx
+++ src/list/chatList/addUser/AddUser.jsx
@@ -1,20 +1,96 @@
-import React from 'react'
+import React, { useState } from 'react'
 import avtar from '../../../assets/avatar.png'
+import { arrayUnion, collection,  doc,  getDocs, query, serverTimestamp, setDoc, updateDoc, where } from "firebase/firestore";
+import { db } from '../../../lib/firebase';
+import { useUserStore } from '../../../lib/userStore';
 
 const AddUser = () => {
+
+   const [user,setUser] = useState(null)
+   const {currentUser} = useUserStore()
+
+  const handleSearch =async (e)=>{
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      const userName = formData.get("userName")
+
+      try{
+        // Create a reference to the cities collection
+        const userRef = collection(db, "users");
+
+        // Create a query against the collection.
+          const q = query(userRef, where("Username", "==", userName));
+          const querySnapShot = await getDocs(q)
+          if(!querySnapShot.empty){
+             setUser(querySnapShot.docs[0].data())
+          }
+
+      }catch(err){
+        console.log(err);
+      }
+  }
+
+  const handleAdd = async () =>{
+  
+    const chatRef = collection(db,"chats")
+    const userchatsRef = collection (db,"userChat")
+
+    try{
+      const newChatRef = doc(chatRef)
+      await setDoc(newChatRef,{
+        createdAt:serverTimestamp(),
+        messages:[],
+      });
+
+     let update= await updateDoc (doc(userchatsRef,user.id),{
+        chats:arrayUnion({
+          chatId:newChatRef.id,
+          lastMessage:"",
+          receiverId :currentUser.id,
+          updateAt : Date.now(),
+        })
+      })
+      console.log(update);
+
+      let update2 =await updateDoc (doc(userchatsRef,currentUser.id),{
+        chats:arrayUnion({
+          chatId:newChatRef.id,
+          lastMessage:"",
+          receiverId :user.id,
+          updateAt : Date.now(),
+        })
+      })
+      console.log(update2);
+     
+
+    }catch(err){
+        console.log(err);
+    }
+  }
+
   return (
-    <div className='bg-orange-500/70 absolute top-0 bottom-0 left-0 right-0 m-auto p-8 w-max h-max rounded-2xl'>
-      <div className="search flex gap-6">
+    <div className='bg-orange-500/80 absolute top-0 bottom-0 left-0 right-0 m-auto p-8 w-max h-max rounded-2xl'>
+      <form className="search flex gap-6" onSubmit={handleSearch}>
         <input type="text" name='userName' placeholder='Enter Username' className='p-2 rounded-lg outline-none text-black'/>
-        <button className='bg-blue-950 p-2 rounded-lg'>Search</button>
-      </div>
+        <button className='bg-blue-950 p-2 rounded-lg' >Search</button>
+      </form>
 
+     {user && <div className="user">
       <div className="details flex justify-around items-center my-4 ">
-        <img src={avtar} alt="" className='rounded-full w-10 h-10' />
-        <h1 className='font-bold text-black text-2xl'>John doe</h1>
+        <img src={user.avatar || avtar} alt="" className='rounded-full w-10 h-10' />
+        <h1 className='font-bold text-white bg-slate-500 p-3 rounded-lg text-2xl'>
+          {user.Username}
+        </h1>
       </div>
+      <button className='bg-blue-950 px-8 py-3 ml-[30%] rounded-lg cursor-progress text-xl' onClick={handleAdd}>Add User</button>
+      </div>}
 
-      <button className='bg-blue-950 p-3 ml-[35%] rounded-lg cursor-progress'>Add User</button>
+      {
+        (!user) && <h1 className='ml-6 mt-4 text-xl font-bold'>OOPS!! No user ..</h1>
+      }
+     
+
+
     </div>
   )
 }
